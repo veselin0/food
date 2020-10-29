ac04f2f1de8bd976cb4d6b2c2aa2d1fdd75b8aee
Veselin <vesselin60@gmail.com>
2020-10-29 16:32:19 +0200

    90. Architecture Approach

---
+++ src/components/Search Bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function SearchBar() {
+  return (
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
