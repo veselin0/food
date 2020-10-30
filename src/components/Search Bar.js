47e5a81018aa18bdc8c48ee553310bd355b0a777
Veselin <vesselin60@gmail.com>
2020-10-30 11:02:54 +0200

    icons setup completed

--- src/components/Search Bar.js
+++ src/components/Search Bar.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { View, TextInput, StyleSheet, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 export default function SearchBar() {
   return (
     <View style={styles.backgroundStyle}>
-      <Image
-        source={require('../imgs/magnifying-glass.png')}
-        style={styles.img}
-      />
-      <TextInput
-        style={styles.inputStyle}
-        placeholder="Search" />
+      <Icon name="search" style={styles.iconStyle} />
+      <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   backgroundStyle: {
+    marginTop: 16,
     backgroundColor: '#d3d3d3',
     height: 60,
     borderRadius: 5,
@@ -24,12 +21,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   inputStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
     flex: 1,
+    fontSize: 22,
   },
-  img: {
-    width: 25,
-    height: 25,
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 16,
   },
 });
