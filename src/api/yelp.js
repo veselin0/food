a0b9b366b195c28477ef4f275d8a4d3a7175bca0
Veselin <vesselin60@gmail.com>
2020-10-30 14:36:08 +0200

    97. Configuring Axios

---
+++ src/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default axios.creeate({
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Autorization:
+      'Berarer GCkav9NsdYoD9V9IBIt8BoPO07aHJ7D--b84wfwZieKbpvznuPU7V6u4q8_Q62FV0IiRyC6zRAjlO6atBPnmKS8WbmQQEJAhKcsilGp9dkSZGfkqWP6p6ntYnLuaX3Yx',
+  },
+});
